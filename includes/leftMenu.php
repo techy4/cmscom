8227d66deaa9c5883edcab044e51be9b78e60ffd
santospanda4 <santospanda4@gmail.com>
2013-10-07 22:10:14 +0530

    Link Name Changed

--- includes/leftMenu.php
+++ includes/leftMenu.php
@@ -1,7 +1,7 @@
 <ul id="dashboard-menu" class="topSpace">
        <li><a href="../Dashboard/dashboard.php"><i class="glyphicon glyphicon-home"></i> <span>Dashboard</span></a></li>
        <li><a href="../CMS/AddLinks.php"><i class="glyphicon glyphicon-plus-sign"></i> <span>Manage Links</span></a></li>    
-       <li><a href="../CMS/MngContents.php"><i class="glyphicon glyphicon-plus-sign"></i><span>Manage Contents</span></a></li>      
+       <li><a href="../CMS/MngContentsAdv.php"><i class="glyphicon glyphicon-plus-sign"></i><span>Manage Contents</span></a></li>      
        <li><a href="#"><i class="glyphicon glyphicon-shopping-cart"></i><span>Manage Home Page</span></a></li>
        <li> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-file"></i> <span>Reports</span></a>
        	<ul class="submenu">
